0279eeb0a43bea81e9866a14d5cd6ed898c98ab5
Nguyễn Thành Nhân <nanipro800@yahoo.com>
2021-09-18 01:53:03 +0700

    Update Bai_2.java

--- Lab_1/src/Bai_2.java
+++ Lab_1/src/Bai_2.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class Bai_2 {
 
 	public static void main(String[] args) {
-		int d,r,CV,DT,canh_nho_nhat;
+		double d,r,CV,DT,canh_nho_nhat;
 		Scanner canh_hcn = new Scanner(System.in);
 		System.out.print("Nhập chiều dài: ");
-		d = canh_hcn.nextInt();
+		d = canh_hcn.nextDouble();
 		System.out.print("Nhập chiều rộng: ");
-		r = canh_hcn.nextInt();
+		r = canh_hcn.nextDouble();
 		CV = (d+r)*2;
 		DT = d*r;
 		canh_nho_nhat = Math.min(d, r);
-		System.out.printf("Chu vi HCN là: %d \n", CV);
-		System.out.printf("Diện tích HCN là: %d \n", DT);
-		System.out.printf("Cạnh nhỏ nhất là: %d", canh_nho_nhat);
+		System.out.printf("Chu vi HCN là: %.2f \n", CV);
+		System.out.printf("Diện tích HCN là: %.2f \n", DT);
+		System.out.printf("Cạnh nhỏ nhất là: %.2f", canh_nho_nhat);
 	}
 
 }
